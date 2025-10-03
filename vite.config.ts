c2248cb7ef0002f8cd19dc1b77ec89d05f5dce4b
Tony Hoang <tt.hoang2108@gmail.com>
2025-10-03 17:27:26 -0500

    added script

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/itmtax',
+	base: '/itm-tax-website',
 	server: {
 		host: true,
 		port: 8080,
