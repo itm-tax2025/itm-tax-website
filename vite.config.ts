621ec4021ddf7ece622518e0be150541c48dc3fd
Tony Hoang <tt.hoang2108@gmail.com>
2025-10-03 17:45:15 -0500

    clear to deploy again

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/itm-tax-website/',
+	base: '/itm-tax-website',
 	server: {
 		host: true,
 		port: 8080,
